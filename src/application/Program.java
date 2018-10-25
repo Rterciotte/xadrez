65cd5dac500b0c1d851fce7edfc387be22998a96
Rogério Terciotte <rterciotte@gmail.com>
2018-10-25 11:30:51 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,10 +33,16 @@ public class Program {
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece capturedPiece = chessMatch.performChessMOve(source, target);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
-				if(capturedPiece != null) {
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
@@ -50,7 +56,5 @@ public class Program {
 		}
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
-		
 	}
-
 }
